2527c276c9ebecdad36167d06b87bbefce42e1b4
Nick Dyer <nick.dyer@atos.net>
2015-06-01 12:26:52 +0100

    Removes ntwitter since deprecated, adds twit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require('express'),
     streamHandler = require('./utils/streamHandler');
 
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
