7af8561be953c04616a7e3dad32c83ddf6c4c01c
Nick Dyer <nick.dyer@atos.net>
2015-06-01 11:44:50 +0100

    Complete main TweetsApp component

--- components/TweetsApp.react.js
+++ components/TweetsApp.react.js
@@ -1,16 +1,137 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var Tweets = require('./Tweets.react.js');
+var Loader = require('./Loader.react.js');
+var NotificationBar = require('./NotificationBar.react.js');
+
+
 module.exports = TweetsApp = React.createclass({
 
   render: function() {
 
     return (
-      <div className="tweets-app">
+        <div className="tweets-app">
         <Tweets tweets={this.state.tweets} />
         <Loader paging={this.state.paging} />
         <NotificationBar count={this.state.count} onShowNewTweets={this.showNewTweets}/>
-      </div>
-    )
+        </div>
+        )
+
+  },
+
+  getInitialState: function(props) {
+
+    props = props || this.props;
+
+    return {
+      tweets: props.tweets,
+      count: 0, 
+      page: 0, 
+      paging: false,
+      skip: 0,
+      done: false
+    };
+
+  },
+
+  componentWillReceiveProps: function(newProps, oldProps) {
+    this.setState(this.getInitialState(newProps));
+  },
+
+  componentDidMount: function() {
+
+    var self = this;
+    var socket = io.connect();
+
+    socket.on('tweet', function(data) {
+      self.addTweet(data);
+    });
+
+    window.addEventListener('scroll', this.checkWindowScroll);
+  },
+
+  addTweet: function(tweet) {
+
+    var updated = this.state.tweets;
+
+    var count = this.state.count + 1;
+
+    var skip = this.state.skip + 1;
+
+    updated.unshift(tweet);
+
+    this.setState({tweets: updated, count: count, skip: skip});
+
+  },
+
+  showNewTweets: function() {
+
+    var updated = this.state.tweets;
+
+    updated.forEach(function(tweet){
+      tweet.active = true;
+    });
+
+    this.setState({tweets: updated, count: 0});
+
+  },
+
+  checkWindowScroll: function() {
+
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var s = documetn.body.scrollTop;
+    var scrolled = (h + s) > document.body.offsetHeight;
+
+    if(scrolled && !this.state.paging && !this.state.done) {
+
+      this.setState({paging: true, page: this.state.page + 1});
+
+      this.getPage(this.state.page);
+
+    }
+  },
+
+  getPage: function(page) {
+
+    var request = new XMLHttpRequest(), self = this;
+    request.open('GET', 'page/' + page + "/" + this.state.skip, true);
+    request.onload = function() {
+
+      if(request.status >= 200 && request.status < 400) {
+        self.loadPagedTweets(JSON.parse(request.responseText));
+      } else {
+        self.setState({paging: false, done: true});
+      }
+    };
+
+    request.send();
+
+  },
+
+  loadPagedTweets: function(tweets) {
+
+    var self = this;
+
+    if(tweets.length > 0) {
+
+      var updated = this.state.tweets;
+
+      tweets.forEach(function(tweet) {
+        updated.push(tweet);
+      });
+
+      setTimeout(function() {
+        self.setState({tweets: updated, paging: false});
+      }, 1000);
+
+    } else {
+
+      this.setState({done: true, paging: false});
+
+    }
 
   }
 
 });
-        
+
